ec9193110aaaab4224d3aa5a49921cfa277dc95f
Dipsy Wong <ycwongal@connect.ust.hk>
2021-07-20 00:45:37 +0800

    validate

--- src/ttt/router.ts
+++ src/ttt/router.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express'
-import { AppContext, Move } from './common'
+import { AppContext, Move, TicTacToeActionType } from './common'
 import axios from 'axios'
 import { generateBattlesForGrading } from './core'
-import { getBattle, publishMove, subscribeMove } from './store'
+import { getBattle, publishOutgoingMove, subscribeOutgoingMove } from './store'
 import { withHandleGameError } from '../tic-tac-toe/withHandleGameError'
 import { v4 } from 'uuid'
 
@@ -27,30 +27,44 @@ export const makeRouter = (appContext: AppContext) => {
   ticTacToeRouter.get('/start/:battleId', async (req, res) => {
     const { battleId } = req.params
     const battle = await getBattle(appContext.pubRedis, battleId)
+    if (battle === null ){
+      res.status(404).send({error: 'battle not found'})
+      return
+    }
     res.setHeader('Cache-Control', 'no-cache')
     res.setHeader('Content-Type', 'text/event-stream')
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Connection', 'keep-alive')
     res.flushHeaders()
-    const state = battle.history[0]
-    res.write(`data: ${JSON.stringify({ youAre: state.externalPlayer, id: battleId })}\n\n`)
+    const moveId = v4()
+    await appContext.incomingMoveQueue.add(moveId, { action: { type: TicTacToeActionType.START_GAME },
+      battleId,
+      by: battle.externalPlayer,
+      id: moveId
+    })
+    res.write(`data: ${JSON.stringify({ youAre: battle.externalPlayer, id: battleId })}\n\n`)
 
-    subscribeMove(appContext.subRedis, battleId, (move) => {
-      res.write(`data: ${JSON.stringify(move.action)}\n\n`)
+    subscribeOutgoingMove(appContext.subRedis, battleId, (move) => {
+      res.write(`data: ${JSON.stringify({ ...move.action, player: move.by })}\n\n`)
     }).catch(e => {
       console.error(e)
       res.status(500).send(e.message)
     })
   })
 
-  ticTacToeRouter.post('/play/:battleId', withHandleGameError((req, res) => {
+  ticTacToeRouter.post('/play/:battleId', withHandleGameError(async (req, res) => {
     const { battleId } = req.params
+    const battle = await getBattle(appContext.pubRedis, battleId)
+    if (battle === null) {
+      res.status(404).send({error: 'Battle not found'})
+      return
+    }
     const { x, y, action } = req.body
     const moveId = v4()
-    const move: Move = { id: moveId, battleId, action: { x, y, action } }
-    appContext.moveQueue.add(moveId, move)
+    const move: Move = { id: moveId, battleId, action: { type: action, x, y }, by: battle.externalPlayer }
+    appContext.incomingMoveQueue.add(moveId, move)
       .then(() =>
-        publishMove(appContext.pubRedis, battleId, move)
+        publishOutgoingMove(appContext.pubRedis, move)
       )
       .then(() => {
         res.json({ message: 'received your command', moveId })
@@ -61,13 +75,13 @@ export const makeRouter = (appContext: AppContext) => {
       })
   }))
 
-  ticTacToeRouter.get('/view/:id', withHandleGameError((req, res) => {
+  ticTacToeRouter.get('/view/:id', withHandleGameError(async (req, res) => {
     let id = req.params.id
-    const game = getBattle(appContext.pubRedis, id)
+    const game = await getBattle(appContext.pubRedis, id)
     res.send(`<pre>${JSON.stringify(game, null, 2)}</pre>`)
   }))
 
-  ticTacToeRouter.post('/', (req,res) => {
+  ticTacToeRouter.post('/', (req, res) => {
     console.log('/')
     res.send('OK')
   })
