12c9d7cc0d6154498d1689212e1be39ee7cc9397
Dipsy Wong <ycwongal@connect.ust.hk>
2021-08-08 22:20:42 +0800

    fix candidate https

--- src/ttt/candidate.ts
+++ src/ttt/candidate.ts
@@ -1,4 +1,5 @@
 import http from 'http'
+import https from 'https'
 import { TicTacToeActionType, TicTacToeState } from './types'
 import { internalizeAction, externalizeAction, applyAction } from './common'
 import { initState } from './config'
@@ -17,40 +18,41 @@ export const candidate = (battleId: string) => {
       axios.post(`${arenaUrl}/${game}/play/${battleId}`, payload)
     }, 500)
   }
-  const req = http.get(`${arenaUrl}/${game}/start/${battleId}`, res => {
-    let state = initState()
-    let me: unknown | undefined
-    res.on('data', data => {
-      const text = new TextDecoder('utf-8').decode(data)
-      const value = JSON.parse(text.replace('data: ', ''))
-      if (value.action === 'flipTable' || value.winner !== undefined) {
-        req.end()
-      } else if (value.youAre !== undefined) {
-        me = value.youAre
-        if (me === 'O' || me === 'first') {
-          const react = agent(state)
-          play({ ...externalizeAction(react), action: react.type })
-        }
-      } else if (value.action !== undefined) {
-        if (valid(state, value, value.player)) {
-          state = apply(state, internalizeAction(value))
-          if (value.player !== me) {
+  const req = (arenaUrl.startsWith('https://') ? https : http)
+    .get(`${arenaUrl}/${game}/start/${battleId}`, res => {
+      let state = initState()
+      let me: unknown | undefined
+      res.on('data', data => {
+        const text = new TextDecoder('utf-8').decode(data)
+        const value = JSON.parse(text.replace('data: ', ''))
+        if (value.action === 'flipTable' || value.winner !== undefined) {
+          req.end()
+        } else if (value.youAre !== undefined) {
+          me = value.youAre
+          if (me === 'O' || me === 'first') {
             const react = agent(state)
-            if (react.type === TicTacToeActionType.END_GAME) {
-              req.end()
-            } else {
-              play({ ...externalizeAction(react), action: react.type })
+            play({ ...externalizeAction(react), action: react.type })
+          }
+        } else if (value.action !== undefined) {
+          if (valid(state, value, value.player)) {
+            state = apply(state, internalizeAction(value))
+            if (value.player !== me) {
+              const react = agent(state)
+              if (react.type === TicTacToeActionType.END_GAME) {
+                req.end()
+              } else {
+                play({ ...externalizeAction(react), action: react.type })
+              }
             }
+          } else {
+            if (timeout !== undefined) {
+              clearTimeout(timeout)
+            }
+            play({ action: TicTacToeActionType.FLIP_TABLE })
           }
-        } else {
-          if (timeout !== undefined) {
-            clearTimeout(timeout)
-          }
-          play({ action: TicTacToeActionType.FLIP_TABLE })
         }
-      }
+      })
     })
-  })
 }
 
 function valid(state: TicTacToeState, value: any, by: any) {
