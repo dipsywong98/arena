038cbda6c773f437d3852a2a2d516feafb8a898f
Dipsy Wong <ycwongal@connect.ust.hk>
2021-08-11 22:31:38 +0800

    ok pushing in progress score is not ok

--- src/quoridor/processConclude.ts
+++ src/quoridor/processConclude.ts
@@ -1,7 +1,7 @@
 import { getBattle, getRun, setRun } from './store'
 import { QuoridorBattle } from './types'
 import redis from '../common/redis'
-import { difference } from 'ramda'
+import { difference, mergeAll } from 'ramda'
 import { reportScore } from '../common/reportScore'
 import { ConcludeRequest } from '../common/types'
 
@@ -17,20 +17,24 @@ export const processConclude = async (concludeRequest: ConcludeRequest) => {
   const battles = (await Promise.all(
     run.battleIds.map(battleId => getBattle(redis, battleId))
   )).filter(battle => battle?.score !== undefined) as Array<QuoridorBattle & { score: number }>
-  const done = battles.map(b => b.id)
-  const missing = difference(run.battleIds, done)
-  // if (battles.length !== run.battleIds.length) {
-  //   return {
-  //     message: `run ${run.id} has not finished its evaluation`,
-  //     done,
-  //     missing: missing
-  //   }
-  // }
+  if (battles.length !== run.battleIds.length) {
+    const done = battles.map(b => b.id)
+    const missing = difference(run.battleIds, done)
+    return {
+      message: `run ${run.id} has not finished its evaluation`,
+      done,
+      missing: missing
+    }
+  }
   const totalScore = battles.map(battle => battle.score).reduce((a, b) => a + b, 0)
-  const totalMessage = battles.map(battle => {
-    return `${battle.id}: ${battle.flippedReason ?? `scored ${battle.score}`}`
-  }).join('\n---------------\n') + missing.map(battleId => `${battleId}: processing`)
-  await reportScore(run.callbackUrl, run.id, totalScore, totalMessage)
+  const totalMessage = mergeAll(battles.map(battle => {
+    return { [battle.id]: `${battle.flippedReason ?? `scored ${battle.score}`}` }
+  }))
+  await reportScore(
+    run.callbackUrl,
+    run.id,
+    totalScore,
+    Object.entries(totalMessage).map(([k, v]) => `${k}: ${v}`).join('\n'))
   const concludedRun = { ...run, score: totalScore, message: totalMessage, completedAt: Date.now() }
   await setRun(redis, concludedRun)
   return concludedRun
